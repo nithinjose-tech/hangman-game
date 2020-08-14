6bbcbc36c2ad96578edd5fbeb0e32f86b139131b
nithinjose-tech <ninjos8@gmail.com>
2020-08-14 20:49:04 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+         <h1>Hangman</h1>
+         <h3>Powered by Nithionite</h3>
+         <p>Find the hideen word - Enter a letter</p>   
+        </>
+    )
+}
+
+export default Header
